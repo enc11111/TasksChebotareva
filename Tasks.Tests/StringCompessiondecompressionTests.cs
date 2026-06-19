e3516766cf56713a74501d704cc2aead8a3c2bf2
enc11111 <mypostcom777@rambler.ru>
2026-06-19 21:06:47 +0300

    String Compression / Decompression ver 1

---
+++ Tasks.Tests/StringCompessiondecompressionTests.cs
@@ -0,0 +1,70 @@
+using Tasks;
+namespace Tasks.Tests;
+
+public class StringCompressionDecompressionTests
+{
+    [Fact]
+    public void StringCompressionOneLetterTest1()
+    {
+        var strOrig = "raaaybbcccdde";
+        var res = StringCompressionDecompression.StringCompress(strOrig);
+        Assert.Equal("ra3yb2c3d2e", res);
+    }
+    [Fact]
+    public void StringCompressionTheSameLetterTest2()
+    {
+        var strOrig = "rrrrrrrrrrrrr";
+        var res = StringCompressionDecompression.StringCompress(strOrig);
+        Assert.Equal("r13", res);        
+    }
+    [Fact]
+    public void StringCompressionEmptyStrTest3()
+    {
+        string strOrig = string.Empty;
+        var res = StringCompressionDecompression.StringCompress(strOrig);
+        Assert.Equal(string.Empty, res);        
+    }
+    [Fact]
+    public void StringCompressionOnlyOneLetterTest4()
+    {
+        string strOrig = "a";
+        var res = StringCompressionDecompression.StringCompress(strOrig);
+        Assert.Equal("a", res);        
+    }
+    [Fact]
+     public void StringDecompressionOneLetterTest5()
+    {
+        var strOrig = "ra3yb2c3d2e";
+        var res = StringCompressionDecompression.StringDecompress(strOrig);
+        Assert.Equal("raaaybbcccdde", res);        
+    }
+    [Fact]
+     public void StringDecompressEmptyStrTest6()
+    {
+        string strOrig = string.Empty;
+        var res = StringCompressionDecompression.StringDecompress(strOrig);
+        Assert.Equal(string.Empty, res);        
+    }
+    [Fact]
+     public void StringDecompressionTheSameLetterTest7()
+    {
+        var strOrig = "r13";
+        var res = StringCompressionDecompression.StringDecompress(strOrig);
+        Assert.Equal("rrrrrrrrrrrrr", res);        
+    }
+    [Fact]
+    public void StringCompressionOnlyOneLetterTest8()
+    {
+        string strOrig = "a";
+        var res = StringCompressionDecompression.StringDecompress(strOrig);
+        Assert.Equal("a", res);        
+    }
+    [Fact]
+    public void StringCompressionDecompressionRepetitionsTest9()
+    {
+        string strOrig = "abbaaca";
+        var resCompressed = StringCompressionDecompression.StringCompress(strOrig);
+        var resDecompressed = StringCompressionDecompression.StringDecompress(resCompressed);
+        Assert.Equal(strOrig, resDecompressed);        
+    }
+}
---
+++ Tasks/StringCompressionDecompression.cs
@@ -0,0 +1,87 @@
+﻿namespace Tasks;
+
+public class StringCompressionDecompression
+{
+	 public static string StringCompress(string strOrig)
+        {
+            string strCompressed = string.Empty;
+            if(strOrig != null && strOrig.Length > 0)
+            {
+                strCompressed += strOrig[0].ToString();
+                int i = 0,
+                    count = 0;
+                for(i = 1; i < strOrig.Length; i++)
+                {
+                    if(strOrig[i] == strOrig[i - 1])
+                    {
+                        count++;
+                        if(i == strOrig.Length - 1)
+                            strCompressed += (count + 1).ToString();
+                    }
+                    else
+                    {
+                        if(count > 0)
+                        {
+                            strCompressed += (count + 1).ToString();
+                            count = 0;
+                        }
+                        strCompressed += strOrig[i];
+                    }
+                }
+            }
+            return strCompressed;
+        }
+      public static string StringDecompress(string strCompressed)
+		{
+			string strDecompressed = string.Empty;
+			if(strCompressed != null && strCompressed.Length > 0)
+			{
+				int i = 0,
+					j = 0,
+                    curNumber = 0;
+				string strNumber = string.Empty;
+                for(i = 0; i < strCompressed.Length; i++)
+				{
+					if(strCompressed[i] >= '0' && strCompressed[i] <= '9')
+					{
+                        strNumber += strCompressed[i];
+                        if(i == strCompressed.Length - 1)
+                        {
+                            if(strNumber != null && strNumber.Length > 0)
+                            {
+						        try
+                                {
+                                    curNumber = Convert.ToInt32(strNumber);
+                                }
+                                catch{}
+						        for(j = 0; j < curNumber - 1; j++)
+						        {
+							        strDecompressed += strCompressed[i - (strNumber.Length)];
+						        }
+                            }
+                        }
+                    }
+					else
+					{
+                        if(strNumber != null && strNumber.Length > 0)
+                        {
+						    try
+                            {
+                                curNumber = Convert.ToInt32(strNumber);
+                            }
+                            catch{}
+						    for(j = 0; j < curNumber - 1; j++)
+						    {
+							    strDecompressed += strCompressed[i - (strNumber.Length + 1)];
+						    }
+                        }
+                        strDecompressed += strCompressed[i];
+
+                        curNumber = 0;
+                        strNumber = string.Empty;
+					}
+				}
+			}
+			return strDecompressed;
+		}
+}
